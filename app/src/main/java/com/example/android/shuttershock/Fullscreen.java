6e5ea14801da47c1c438e334f5b230b38b6e4dcc
nshort <chellchell5321@aol.com>
2016-02-15 16:15:45 -0500

    Create Fullscreen.java
    activity to show fullscreen imageview

---
+++ app/src/main/java/com/example/android/shuttershock/Fullscreen.java
@@ -0,0 +1,16 @@
+package com.example.android.shuttershock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by shortn1 on 2/15/2016.
+ */
+public class Fullscreen extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_fullscreen);
+    }
+}
